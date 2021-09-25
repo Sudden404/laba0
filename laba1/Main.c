9a1e6865706252722b1285cef1cf0e102fad27d9
Sudden404 <90724604+Sudden404@users.noreply.github.com>
2021-09-25 21:36:05 +0300

    heh

--- laba1/Main.c
+++ laba1/Main.c
@@ -4,18 +4,32 @@
 
 int main()
 {
+	int b;
+	int idiot;
 	int input1 = 0, input2 = 0, input3 = 0, ost = 0;
 	//input(1,2,3) - input values
-	scanf("%d %d %d", &input1, &input2, &input3);
-	if (input1 == 0) //|| (input2 == 0)) {
-		//if (input1 == 0) 
+	idiot = scanf("%d %d %d", &input1, &input2, &input3);
+	b = input3 / 1;
+	if (
+		(
+			(idiot < 3) && (idiot < 1) &&//check on only 1 char variable
+			(b == "0") && (input3 != 0)//check on input3 == char
+			)
+		|| (idiot == 3)
+		)
+	{
+		if (input1 == 0) //|| (input2 == 0)) {
+			
 			printf("if u belive in rocket science use 0 else change values\n");
 		//else printf("if u belive in rocket science use  else change values\n");
 //}
-	else
-	{
-		ost = input2 % input1;//ost - remainder of the division
-		printf(" %d\n", ost);
+		else
+		{
+			ost = input2 % input1;//ost - remainder of the division
+			printf(" remainder of division=%d\n %d\n ", ost, idiot);
+		}
 	}
+	else  printf("How i will calculated words ???\n %d", b);
+	
 	return 0;
 }
